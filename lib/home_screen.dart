a465a1eb81de6267b63beae0d9dac10d2b4bc7cf
João Gabriel <jgmoreira15@gmail.com>
2020-07-26 07:20:53 -0300

    Botoes adicionados na tela inicial

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -20,6 +20,25 @@ class _HomeScreenState extends State<HomeScreen> {
             Icon(Icons.school, size: 100, color: Colors.white, ),
             Padding(
               padding: EdgeInsets.only(bottom: 100.0),
+            ),
+           ButtonTheme(
+             minWidth: 150.0,
+             height: 50.0,
+             child:  RaisedButton(
+               onPressed: (){}, child: Text("Começar", style: TextStyle(color: Colors.white, fontSize: 20.0),),
+               color: Colors.black45,
+             ),
+           ),
+            Padding(
+              padding: EdgeInsets.only(top: 15.0),
+            ),
+            ButtonTheme(
+              minWidth: 150.0,
+              height: 50.0,
+              child:  RaisedButton(
+                onPressed: (){}, child: Text("Sobre", style: TextStyle(color: Colors.white, fontSize: 20.0),),
+                color: Colors.black45,
+              ),
             )
           ],
         ),
